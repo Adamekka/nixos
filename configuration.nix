59e9e1c962888c410f693c8e7a2fed26dd32c75c
Adam Cvikl <adam.wasd4@gmail.com>
2023-08-08 00:42:12 +0200

    enable `auto upgrade`

--- configuration.nix
+++ configuration.nix
@@ -167,6 +167,11 @@
   sound.enable = true;
 
   system = {
+    autoUpgrade = {
+      allowReboot = true;
+      channel = "https://channels.nixos.org/nixos-unstable";
+      enable = true;
+    };
     copySystemConfiguration = true;
     stateVersion = "23.05";
   };
