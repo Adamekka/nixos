539ecf5ff1bb895733aeac3d55deae924e00e6f1
Adam Cvikl <adam.wasd4@gmail.com>
2023-08-08 00:52:17 +0200

    cleanup

--- configuration.nix
+++ configuration.nix
@@ -45,7 +45,7 @@
       neovim
       ninja
       nixpkgs-fmt
-      nur.repos.nltch.spotify-adblock
+      # nur.repos.nltch.spotify-adblock
       obs-studio
       onefetch
       pfetch
@@ -109,11 +109,11 @@
 
   nixpkgs.config = {
     allowUnfree = true;
-    packageOverrides = pkgs: {
-      nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-        inherit pkgs;
-      };
-    };
+    # packageOverrides = pkgs: {
+    #   nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+    #     inherit pkgs;
+    #   };
+    # };
     permittedInsecurePackages = [
       #"openssl-1.1.1u"
       "openssl-1.1.1v"
