53ca73fcae57e2e3c2b12cce5adbd6b5a30d033e
Adam Cvikl <adam.wasd4@gmail.com>
2023-08-10 12:40:31 +0200

    cleanup

--- configuration.nix
+++ configuration.nix
@@ -111,15 +111,6 @@
     wireless.userControlled.enable = true;
   };
 
-  nix.settings = {
-    substituters = [
-      "https://nix-gamig.cachix.org"
-    ];
-    trusted-public-keys = [
-      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-    ];
-  };
-
   nixpkgs.config = {
     allowUnfree = true;
     # packageOverrides = pkgs: {
