c60c7860dc9ab968de9032c74c633e8b60973d67
Adam Cvikl <adam.wasd4@gmail.com>
2023-08-10 13:31:38 +0200

    add `Spotify`

--- configuration.nix
+++ configuration.nix
@@ -47,7 +47,7 @@
       nethogs
       ninja
       nixpkgs-fmt
-      # nur.repos.nltch.spotify-adblock
+      nur.repos.nltch.spotify-adblock
       nvtop
       obs-studio
       onefetch
@@ -114,11 +114,11 @@
 
   nixpkgs.config = {
     allowUnfree = true;
-    # packageOverrides = pkgs: {
-    #   nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-    #     inherit pkgs;
-    #   };
-    # };
+    packageOverrides = pkgs: {
+      nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+        inherit pkgs;
+      };
+    };
     permittedInsecurePackages = [
       #"openssl-1.1.1u"
       "openssl-1.1.1v"
