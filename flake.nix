6559086f0e5aaa58f25cf013a86a4fe71bb35873
Adam Cvikl <68786400+Adamekka@users.noreply.github.com>
2025-08-18 04:41:16 +0200

    chaotic nyx

--- flake.nix
+++ flake.nix
@@ -1,12 +1,14 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, chaotic, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       modules = [
         ./configuration.nix
+        chaotic.nixosModules.default
       ];
       specialArgs = { inherit inputs; };
       system = "x86_64-linux";
