9da00d4d8550704edeaec12fb169cbba9016ac22
Adamekka <adam.wasd4@gmail.com>
2026-05-20 22:01:21 +0200

    env vars

--- configuration.nix
+++ configuration.nix
@@ -55,8 +55,6 @@
       __GL_VRR_ALLOWED = "1";
       AMD_VULKAN_ICD = "RADV";
       ENABLE_HDR_WSI = "1";
-      GAMEMODERUN = "1";
-      MESA_GLSL_CACHE_MAX_SIZE = "16G";
       MESA_SHADER_CACHE_MAX_SIZE = "16G";
       NIXOS_OZONE_WL = "1";
       PROTON_ENABLE_HDR = "1";
@@ -64,9 +62,6 @@
       PROTON_FSR4_UPGRADE = "1";
       PROTON_USE_NTSYNC = "1";
       PROTON_XESS_UPGRADE = "1";
-      RADV_PERFTEST = "rt,nggc";
-      VKD3D_CONFIG = "dxr,dxr11";
-      WLR_DRM_NO_ATOMIC = "0";
     };
     systemPackages = with pkgs; [
       alacritty
