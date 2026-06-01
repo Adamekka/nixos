0ed665ff31cd30caa656d12b44f94c6e7d26a931
Adamekka <adam.wasd4@gmail.com>
2026-06-01 14:18:16 +0200

    env vars

--- configuration.nix
+++ configuration.nix
@@ -67,6 +67,7 @@
       __GL_VRR_ALLOWED = "1";
       AMD_VULKAN_ICD = "RADV";
       ENABLE_HDR_WSI = "1";
+      DXVK_HDR = "1";
       MESA_SHADER_CACHE_MAX_SIZE = "16G";
       NIXOS_OZONE_WL = "1";
       PROTON_ENABLE_HDR = "1";
